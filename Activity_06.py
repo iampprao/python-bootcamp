ee6432edd5880b477291820e6ebe54e1215475a8
iampprao <88310046+iampprao@users.noreply.github.com>
2021-08-02 23:01:56 +0530

    done commit

---
+++ Activity_06.py
@@ -0,0 +1,12 @@
+a = input('enter the elements')
+b = a.split()
+l=list(b)
+print(l)
+c=b[0:3]
+print("sliced list= ",c)
+b[0]=0
+b[-1]=0
+print("replace list1= ",b)
+c[0]=0
+c[-1]=0
+print("replace list2= ",c)
