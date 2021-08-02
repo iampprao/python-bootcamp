2f0395bf040fced6d0711f4d190cc301879b1d9f
iampprao <88310046+iampprao@users.noreply.github.com>
2021-08-02 14:43:20 +0530

    done commit

--- Activity_03.py
+++ Activity_03.py
@@ -1,5 +1,6 @@
-str1=input("enter the first string: \n")
+str1=input("enter the first string:  ")
 str2=input("enter the second string: ")
-str=str1+str2
+str=str1+" "+str2 + "\n"
 print(str)
-print(str*4)
+print(str*5)
+
