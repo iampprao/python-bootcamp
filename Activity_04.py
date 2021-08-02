0a4bee5fa1791726da3de2fe541b13fe06f8d6cb
iampprao <88310046+iampprao@users.noreply.github.com>
2021-08-02 21:36:12 +0530

    done commit

--- Activity_04.py
+++ Activity_04.py
@@ -2,3 +2,5 @@ a = int(input("enter the first number to be added: "))
 b = int(input("enter the second number to be added: "))
 c = a+ b
 print("the sum of %d and %d is %d\n"%(a,b,c))
+print("{}+{}={}".format(a,b,c))
+print(f"{a}+{b}={c}")
