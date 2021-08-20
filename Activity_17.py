45ecb4a00a0f2f66389e9fb56d3f3b6b310fb935
iampprao <88310046+iampprao@users.noreply.github.com>
2021-08-20 11:11:45 +0530

    updatecommit

--- Activity_17.py
+++ Activity_17.py
@@ -8,7 +8,7 @@ def string1(a):
     b=str()
     for i in a.split('), ('):
         c=i.split(',')
-        b=b+c[0]+"="+c[1]+"; "
+        b=b+c[0]+"="+c[1]+";"
         b=b.rstrip(';')
     b=b.replace('[(','')
     b=b.replace(')]','')
